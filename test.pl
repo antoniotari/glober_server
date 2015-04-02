ed0a0e021587252e956e776b2d482e0d7849a0a3
Antonio Tari <antoniotari11@gmail.com>
2015-04-02 15:04:54 -0400

    test.pl

--- test.pl
+++ test.pl
@@ -1,17 +1,6 @@
 #!/usr/bin/perl -w
 print "Content-type: text/html\r\n\r\n";
-
-
 print "Hello there!<br />\nJust testing .<br />\n";
-
-
-for ($i=0; $i<10; $i++)
-
-
-{
-
-
-print $i."<br />";
-
-
+for ($i=0; $i<10; $i++){
+	print $i."<br />";
 }
